11fa90095483122b78de12efb9ba22bb65056eb9
palash vijay <palashvijay13319@gmail.com>
2015-01-10 19:29:05 +0530

    Added a script to install chrome on Linux

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ChromeOnLinux
 Script to install the Latest vesrion of Google Chrome within few seconds.
+
+All you need to do is open your terminal
+and then run  bash "path/to/install_google_chrome.sh" without quotes.
---
+++ install_google_chrome.sh
@@ -0,0 +1,25 @@
+#Super simple bash script to install google chrome
+#without worrying about your system configuration.
+
+#       ===== Author====
+#          Palash vijay
+
+if [[ $(getconf LONG_BIT) = "64" ]]
+then
+	echo "64bit Detected" &&
+	echo "Installing Google Chrome" &&
+	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &&
+	sudo dpkg -i google-chrome-stable_current_amd64.deb &&
+	rm -f google-chrome-stable_current_amd64.deb
+else
+	echo "32bit Detected" &&
+	echo "Installing Google Chrome" &&
+	wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb &&
+	sudo dpkg -i google-chrome-stable_current_i386.deb &&
+	rm -f google-chrome-stable_current_i386.deb
+fi
+
+# checking if the current version is outdated.
+sudo apt-get update &&
+# upgrading to the latest version if the version is not outdated.
+sudo apt-get upgrade
